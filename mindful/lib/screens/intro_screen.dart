f109b63614b91b2165981d6f8015c61f74604d6c
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-29 18:08:53 +0200

    Add: a stack layout and a button

--- mindful/lib/screens/intro_screen.dart
+++ mindful/lib/screens/intro_screen.dart
@@ -6,21 +6,34 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Mindful"),
-      ),
       body: Stack(
         children: [
-          Positioned.fill(child: Image.asset(
+          Positioned.fill(
+              child: Image.asset(
             'assets/Sea.jpg',
             fit: BoxFit.cover,
           )),
-          const Center(
-          child: Text(
-            "Welcome to Mindful",
-            style: TextStyle(fontSize: 24),
+          const Align(
+            alignment: Alignment(0, -0.5),
+            child: Text(
+              "Welcome to Mindful",
+              style: TextStyle(
+                color: Colors.white,
+                shadows: [
+                  Shadow(blurRadius: 5,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  offset: Offset(5, 5))
+                ],
+                fontSize: 24),
+            ),
           ),
-        )],
+          Align(
+            alignment: const Alignment(0, 0.5),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Start')),
+          )
+        ],
       ),
     );
   }
