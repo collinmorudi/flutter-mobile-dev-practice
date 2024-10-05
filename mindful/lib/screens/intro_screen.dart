b8c7175be83ab992670201b117dd606124a38f5a
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-10-05 16:39:36 +0200

    Add: save settings button

--- mindful/lib/screens/intro_screen.dart
+++ mindful/lib/screens/intro_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:mindful/data/sp_helper.dart';
 import 'package:mindful/screens/settings_screen.dart';
 
-class IntroScreen extends StatelessWidget {
+class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  String name = "";
+  String image = "Lake";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final SPHelper helper = SPHelper();
+    helper.getSettings().then((settings) {
+      name = settings["name"] ?? "";
+      image = settings["image"] ?? "Lake";
+
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,32 +33,33 @@ class IntroScreen extends StatelessWidget {
         children: [
           Positioned.fill(
               child: Image.asset(
-            'assets/Sea.jpg',
+            'assets/$image.jpg',
             fit: BoxFit.cover,
           )),
-          const Align(
-            alignment: Alignment(0, -0.5),
+          Align(
+            alignment: const Alignment(0, -0.5),
             child: Text(
-              "Welcome to Mindful",
-              style: TextStyle(
-                color: Colors.white,
-                shadows: [
-                  Shadow(blurRadius: 5,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  offset: Offset(5, 5))
-                ],
-                fontSize: 24),
+              "Welcome $name",
+              style: const TextStyle(
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                        blurRadius: 5,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        offset: Offset(5, 5))
+                  ],
+                  fontSize: 24),
             ),
           ),
           Align(
             alignment: const Alignment(0, 0.5),
             child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (BuildContext context) => const SettingsScreen())
-                );
-              },
-              child: const Text('Start')),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          const SettingsScreen()));
+                },
+                child: const Text('Start')),
           )
         ],
       ),
--- mindful/lib/screens/settings_screen.dart
+++ mindful/lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindful/data/sp_helper.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -11,6 +12,13 @@ class _SettingsScreenState extends State<SettingsScreen> {
   final TextEditingController textName = TextEditingController();
   final List<String> _images = ["Lake", "Mountain", "Sea", "Country"];
   String _selectedImage = "Lake";
+  final SPHelper helper = SPHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    getSettings();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,15 +37,59 @@ class _SettingsScreenState extends State<SettingsScreen> {
             DropdownButton<String>(
               value: _selectedImage,
               items: _images.map((String value) {
-                return DropdownMenuItem(
-                  value: value,
-                  child: Text(value));
+                return DropdownMenuItem(value: value, child: Text(value));
               }).toList(),
-              onChanged: (newValue) {},
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedImage = newValue ?? "Lake";
+                });
+              },
             )
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          saveSetting().then((value) {
+            String message = value
+                ? "Settings saved successfully!"
+                : "Error: Settings were not saved!";
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(message),
+              duration: const Duration(seconds: 3),
+            ));
+          });
+        },
+        child: const Icon(Icons.save),
+      ),
     );
+  }
+
+  Future<bool> saveSetting() async {
+    return await helper.setSettings(String, textName.text, _selectedImage);
+  }
+
+  Future<void> getSettings() async {
+    Map<String, String> settings = await helper.getSettings();
+    _selectedImage = settings["image"] ?? "Lake";
+
+    // Check if the image value exists in the _images list
+    String? image = settings["image"];
+    if (_images.contains(image)) {
+      _selectedImage = image!;
+    } else {
+      _selectedImage = "Lake"; // Default to "Lake" if invalid value is returned
+    }
+
+    textName.text = settings["name"] ?? "";
+
+    setState(() {});
+  }
+
+  // cleanup
+  @override
+  void dispose() {
+    textName.dispose();
+    super.dispose();
   }
 }
