6af5817e82ee8f84f9a73ae2c4fc8612b985922c
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-10-16 21:14:53 +0200

    Initial commit

---
+++ simple_todo/lib/screens/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
+      appBar: AppBar(
+        title: const Text('Simple Todo'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('This is the home page'),
+      ),
+    );
+  }
+}
