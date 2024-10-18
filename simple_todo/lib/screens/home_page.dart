13d54feda8847f74ba4ce2bebb7cdca224f4039d
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-10-18 07:28:52 +0200

    Added a Listview to display todo list items

--- simple_todo/lib/screens/home_page.dart
+++ simple_todo/lib/screens/home_page.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List todoList = [
+    ['Buy milk', false],
+    ['Buy eggs', false],
+    ['Buy bread', false],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple.shade300,
-      appBar: AppBar(
-        title: const Text('Simple Todo'),
-        backgroundColor: Colors.deepPurple,
-        foregroundColor: Colors.white,
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('This is the home page'),
-      ),
-    );
+        backgroundColor: Colors.deepPurple.shade300,
+        appBar: AppBar(
+          title: const Text('Simple Todo'),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                  top: 20, left: 20, right: 20, bottom: 0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.deepPurple,
+                ),
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  todoList[index][0],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    decoration: todoList[index][1]
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
